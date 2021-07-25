6feeeb06040766fbb0995266bf73de81b941dff1
Dion Vitor do Prado <71671413+DionVitor@users.noreply.github.com>
2021-07-24 23:09:36 -0300

    Adicionado model de teams pokemon.

---
+++ core/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from users.models import User
+
+
+class Team(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False, blank=False)
+    pokemons = models.JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Team de {self.user.username}"
